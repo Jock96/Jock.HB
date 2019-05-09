cfabc6978af548ea5ebbef01ddaef2f79c1a2b14
Jock_96 <Peter96pvl@gmail.com>
2019-05-09 18:31:18 +0700

    Правки
    Добавление Background в ресурсы. Мелкие правки по тестам и проекту.

--- Jock.HB.UI/Properties/Resources.Designer.cs
+++ Jock.HB.UI/Properties/Resources.Designer.cs
@@ -59,5 +59,15 @@ namespace Jock.HB.UI.Properties {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Поиск локализованного ресурса типа System.Drawing.Bitmap.
+        /// </summary>
+        public static System.Drawing.Bitmap back {
+            get {
+                object obj = ResourceManager.GetObject("back", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
